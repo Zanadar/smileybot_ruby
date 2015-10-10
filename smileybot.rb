81008b263454d1c87c39b76193315e46e12db19c
Zander Mackie <zander@skilledup.com>
2015-10-10 17:18:58 -0400

    Change port of face_server

--- smileybot.rb
+++ smileybot.rb
@@ -8,7 +8,7 @@ Dotenv.load
 get '/' do
   channel_for_response=params['channel_name']
   face_server = ENV['FACE_SERVER']
-  face = Curl.get("#{face_server}:3000").body
+  face = Curl.get("#{face_server}:5000").body
   face = CGI.unescape(face)
   response = {text: "#{face}", username: "smileybot", icon_emoji: ":ghost:", channel: "##{channel_for_response}"}
   Curl.

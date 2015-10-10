7c76b6196c2aaa15a4e465645802d5498dce2122
Zander Mackie <zander@skilledup.com>
2015-10-10 17:10:19 -0400

    Update .env for correct variable names

--- smileybot.rb
+++ smileybot.rb
@@ -7,11 +7,11 @@ require 'json/ext'
 Dotenv.load
 get '/' do
   channel_for_response=params['channel_name']
-  face_server = ENV['face_server']
+  face_server = ENV['FACE_SERVER']
   face = Curl.get("#{face_server}:3000").body
   face = CGI.unescape(face)
   response = {text: "#{face}", username: "smileybot", icon_emoji: ":ghost:", channel: "##{channel_for_response}"}
   Curl.
-     post(ENV['incoming_webhook'], response.to_json)
+     post(ENV['INCOMING_WEBHOOK'], response.to_json)
 end
 
